8f63e6941fd0feb65c733a194cdb42521a1476df
jiankychen <jiechean@163.com>
2023-10-22 00:49:26 +0800

    Site updated: 2023-10-22 00:49:13

--- js/polyfill.js
+++ js/polyfill.js
@@ -1,3 +1,3 @@
-// build time:Sun Oct 22 2023 00:20:13 GMT+0800 (中国标准时间)
+// build time:Sun Oct 22 2023 00:44:16 GMT+0800 (中国标准时间)
 (function(o){}).call("object"===typeof window&&window||"object"===typeof self&&self||"object"===typeof global&&global||{});
 //rebuild by hrmmi 
